62ec6708bd28fb5e44f632f1e84ed271b7de56dd
David Liberal <davidliberal0@gmail.com>
2025-12-25 19:24:11 -0500

    set up Timer and TimerTask objects. Override TimerTask with counter messages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,26 @@
 package org.example;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        
+        // Java Countdown Timer Program
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            int count = 5;
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+                if (count < 0) {
+                    System.out.println("Happy New Year!");
+                    timer.cancel();
+                }
+            }
+        }; 
     }
 }
